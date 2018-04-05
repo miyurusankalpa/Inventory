7b8778d1a71c4068f73f53b4b481b36482866a3a
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-04-05 23:07:30 +0530

    testing mysql connections

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,9 +1,14 @@
+<%@page import="test.test"%>
 <% String pageTitle="Hello World";  %>
 <jsp:directive.include file = "header.jsp" />
 	  <div class="container">
 	  <h1>
       <%
-         out.println("Hello World 2");
+      out.println("Hello World 2 <br>");
+      
+      test tes = new test();
+      out.println("SQL : "+tes.testc());
+      	
       %></h1>
 	  </div>
    </body>
---
+++ src/test/test.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.sql.*;
+
+public class test {
+	
+	 public String testc() throws ClassNotFoundException {
+		 Connection conn = null;
+		 final String DB_URL = "jdbc:mysql://localhost/database";
+		 
+		 Class.forName("com.mysql.jdbc.Driver");
+		 
+		try {
+			conn = DriverManager.getConnection(DB_URL,"test","test");
+			return "Connected";
+		} catch (SQLException e) {
+			 return "Error, "+e.getMessage();
+		}
+	}
+
+}
