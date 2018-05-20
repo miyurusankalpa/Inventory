e607bda40c59985ef7d77d3de6aeead0449ce7eb
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-05-20 23:15:14 +0530

    sql file with data

--- Script.sql
+++ Script.sql
@@ -14,6 +14,8 @@ CREATE TABLE `oop`.`products` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `products` VALUES (1,'Pen',20,20),(2,'Books',100,50),(3,'Water Bottles',2,70),(4,'Broom',1,100),(5,'Desk',1,1000);
+
 CREATE TABLE `oop`.`users` (
   `idusers` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NULL,
@@ -22,6 +24,7 @@ CREATE TABLE `oop`.`users` (
   PRIMARY KEY (`idusers`));
 
 INSERT INTO oop.users (`idusers`,`username`, `password`) VALUES (null,"test","test");
+INSERT INTO oop.users (`idusers`,`username`, `password`) VALUES (null,"admin","admin");
 
 CREATE TABLE `oop`.`customers` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -31,6 +34,8 @@ CREATE TABLE `oop`.`customers` (
   `contact` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
   
+INSERT INTO `customers` VALUES (1,'Miyuru Sankalpa','No1, Galle Rd, Galle','miyuru@live.com','774020209'),(2,'Sadeeptha Palihawadana','No 5, Mount Laviania, Colombo','sadee@icloud.com','718985557'),(3,'Ravindu Prabashwara','No 165, Tank Rd, Polonnaruwa','ravindu@gmail.com','778685548'),(4,'K Pirabranjan','No 15, Hospital Rd, Colombo','pirabranjan@gmail.com','774565445'),(5,'Shamal Karunanayake','No 58, Pothuhahra, Kurunagala','shamal@gmail.com','77555444');
+
 CREATE TABLE `oop`.`transactions` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `date` VARCHAR(45) NULL,
@@ -38,3 +43,5 @@ CREATE TABLE `oop`.`transactions` (
   `total` INT NULL,
   `customers` INT NULL,
   PRIMARY KEY (`id`));
+  
+  INSERT INTO `transactions` VALUES (1,'2018-05-19',1,2,1),(2,'2018-05-18',5,1,2),(3,'2018-05-17',2,5,3),(4,'2018-05-21',2,2,1),(5,'2018-05-16',3,1,5),(6,'2018-05-08',2,3,5);
