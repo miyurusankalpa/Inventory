fa4d96c011b9de2b1a2967e3a93eb6504cacdb31
Sadeeptha <Sadeeptha@DESKTOP-8U072K4>
2018-05-20 16:21:23 +0530

    Created Customer_Edit serverlet

--- src/Serverlets/Customers_Edit.java
+++ src/Serverlets/Customers_Edit.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import database.Database;
+import modals.Customers;
 
 /**
  * Servlet implementation class Customers_Edit
@@ -23,19 +27,41 @@ public class Customers_Edit extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession(true);
+		
+		Database c = new Database();
+
+		Customers cu = new Customers();
+
+		int d = Integer.parseInt(request.getParameter("id"));
+		String n = request.getParameter("name");
+		String a = request.getParameter("address");
+		String e = request.getParameter("email");
+		int co = Integer.parseInt(request.getParameter("contact"));
+		
+		cu.setId(d);
+		cu.setName(n);
+		cu.setAddress(a);
+		cu.setEmail(e);
+		cu.setContact(co);
+		
+		String query = cu.editquery();
+
+		//out.println(query);c
+
+		String result = c.adddata(query);
+		
+		session.setAttribute("result", result);
+		
+		//response.getWriter().append(result);
+		
+		response.sendRedirect("/Inventory/list/customers.jsp");
+		
+		//out.println(result);
 	}
 
 }
