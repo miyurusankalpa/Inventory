489bea34a6752421ca092abbbd243694be12a6fb
Miyuru Sankalpa <msankalpa@gmail.com>
2019-05-23 23:38:04 +0530

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -34,7 +34,7 @@ CREATE TABLE `oop`.`customers` (
   `contact` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
   
-INSERT INTO `customers` VALUES (1,'Miyuru Sankalpa','No1, Galle Rd, Galle','miyuru@live.com','774020209'),(2,'Sadeeptha Palihawadana','No 5, Mount Laviania, Colombo','sadee@icloud.com','718985557'),(3,'Ravindu Prabashwara','No 165, Tank Rd, Polonnaruwa','ravindu@gmail.com','778685548'),(4,'K Pirabranjan','No 15, Hospital Rd, Colombo','pirabranjan@gmail.com','774565445'),(5,'Shamal Karunanayake','No 58, Pothuhahra, Kurunagala','shamal@gmail.com','77555444');
+INSERT INTO `customers` VALUES (1,'Miyuru','No1','miyuru@test.com','1555685'),(2,'Sadeeptha','No5','sadee@test.com','1235777'),(3,'Ravindu','No16','ravindu@test.com','1255566'),(4,'Pirabranjan','No15','pirabranjan@test.com','225566'),(5,'Shamal','No58','shamal@test.com','1223355');
 
 CREATE TABLE `oop`.`transactions` (
   `id` INT NOT NULL AUTO_INCREMENT,
